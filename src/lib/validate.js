68324e7d656765705a87eae57804cc6c2e191586
khayden73 <keith73@gmail.com>
2021-04-06 22:50:44 -0400

    stylin

--- src/lib/validate.js
+++ src/lib/validate.js
@@ -1,15 +1,14 @@
 export function isValidEmail(email) {
-    // console.log(`[isValidEmail] ${email} : ${email.length > 0} && ${email.includes("@")}`);
-    return email.includes("@");
-    // return /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(email);
+    // return email.includes("@"); // for testing
+    return /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(email);
 }
 
 export function isValidPassword(pw) {
-    return pw.length > 3;
-    // return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/.test(pw);
+    // return pw.length > 3; // for testing
+    return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/.test(pw);
 }
 
-/*
+/* password validation regex
     /^
         (?=.*\d)          // should contain at least one digit
         (?=.*[a-z])       // should contain at least one lower case
